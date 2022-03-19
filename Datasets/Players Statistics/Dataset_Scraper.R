8499b3f91dd9025b67e817631f95496033101a9e
Alberto Allegri <alberto1998@hotmail.it>
2022-03-19 23:33:52 +0100

    Statistics update

--- Datasets/Players Statistics/Dataset_Scraper.R
+++ Datasets/Players Statistics/Dataset_Scraper.R
@@ -1,12 +1,11 @@
-#clear the environment 
 rm(list=ls()) 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-
+  
 #Library for scraping data from FBref.com
 #devtools::install_github("JaseZiv/worldfootballR")
-
-listofpackages <- c("worldfootballR", "dplyr", "devtools", "dplyr", "httr", "stringr")
-
+  
+listofpackages <- c("worldfootballR", "dplyr", "devtools", "dplyr", "httr", "stringr", "plyr")
+  
 for (j in listofpackages){
   if(sum(installed.packages()[, 1] == j) == 0) {
     install.packages(j)
@@ -31,6 +30,7 @@ for (i in (1:numb)){
     print(i)
     
     #First Step, select the player and store FBref and TRM links
+    pos = unlist(c(mapped_players[i,"TmPos"]))
     player_FB = unlist(c(mapped_players[i,"UrlFBref"]))
     player_TRM = unlist(c(mapped_players[i,"UrlTmarkt"]))
     
@@ -44,108 +44,73 @@ for (i in (1:numb)){
     if (isTRUE(dim(new_player_FB)[1] == 0) == TRUE){
       next}
     
-    #Select FBref data for the last 365 days, or, if missing, the most recent one
-    players_data = subset(new_player_FB, scouting_period == "Last 365 Days" | scouting_period == new_player_FB[1,"scouting_period"])
-    #Save scouting period, will be useful later
-    scout_per = unique(players_data$scouting_period)
+    #Let's initialize our dataframes
+    players_data = data.frame() 
+    player_data_mom = data.frame()
     
-    #Transpose the dataframe
-    players_data = data.frame(t(players_data))
+    #And now let's scrape all available years for each player
+    for (i in unique(new_player_FB$scouting_period)){
+      player_data_mom = subset(new_player_FB, scouting_period == i)
+      player_data_mom = data.frame(t(player_data_mom))
+      names(player_data_mom) = c(player_data_mom["Statistic",])
+      player_data_mom = data.frame(player_data_mom["Per90",])
+      player_data_mom$scouting_period = i
+      players_data = rbind.fill(players_data, player_data_mom)
+    }
     
-    #Put the Statistic row as header
-    names(players_data) = c(players_data["Statistic",])
+    #Take the tail of the dataframe, and then exclude "Last 365 Days" report
+    players_data = tail(players_data, n = length(unique(new_player_FB$scouting_period)))   
+    players_data = subset(players_data, scouting_period != "Last 365 Days")
     
-    #Select the statistics
-    players_data = data.frame(players_data["Per90",])
-    
-    #Change index
-    rownames(players_data) <- unlist(c(mapped_players[i,"PlayerFBref"]))
-    
-    #Add various columns
-    players_data$scouting_period = scout_per #Scouting period
-    players_data$Player_valuation = as.integer(unlist(c(new_player_TRM[,"player_valuation"]))) #Valuation
-    
-    #Skip players that do not have valuation
-    if (isTRUE(is.na(players_data$Player_valuation)) == TRUE){
-      next}
-    
-    players_data$Age = new_player_TRM$age #Age
+    #Insert Trasnfermarket link, it will be useful when we'll have to scrape valuation
+    players_data$link_TRM = player_TRM
+    #Insert name of the player
+    players_data$Player = unique(new_player_FB$Player)
+
+    #Now let's add some other important columns
+    players_data$Age = as.integer(ceiling(difftime(paste(substr(players_data[,'scouting_period'],6,9), "-6-10", sep=""), new_player_TRM$date_of_birth, units = "days")/365))
     players_data$Height = new_player_TRM$height #Height
-    players_data$Position = unlist(c(mapped_players[i,"TmPos"])) #Position in the field
+    players_data$Position = pos #Position in the field
     players_data$Foot = new_player_TRM$foot #Preferred foot
-    contr_left = as.Date(new_player_TRM$contract_expires) - as.Date("2022-03-03")
-    players_data$Y_Contract_Left = as.numeric(contr_left, units="days")/365 #Year of contract left
-    players_data$Actual_Club = new_player_TRM$current_club #Actual club (for season 2021/2022)
-    
+
     #To add the league
-    player_stats = fb_player_season_stats(player_FB, stat_type = "playing_time")
-    if (players_data$scouting_period == "Last 365 Days"){
-      player_stats = subset(player_stats, Season == "2021-2022" | Season == "2022" | Season == "2021")
-    } else {
-      player_stats = subset(player_stats, Season == substring(unique(players_data$scouting_period),1,9))
-    }
-    camp = c()
-    for (i in player_stats$Comp){
-      x = substring(i,1,1)
-      if (isTRUE(!is.na(as.numeric(x)) == TRUE)){
-        camp = c(camp, i)
-        } else {next}
-    }
-    campionato = unlist(c(camp))
-    campionato = campionato[campionato != "1. Champions Lg" & 
-                            campionato != "2. Europa Lg" & 
-                            campionato != "3. Conf Lg" &
-                            campionato != "3. 3. Liga"]
-    campionato = tail(campionato, n=1)
-    players_data$Data_League = substring(campionato,4,nchar(campionato)) 
-    #Here, we basically appended a column related to the LEAGUE from which data are scraped.
-    #Indeed, there were some cases where the player at the end of season 2020/2021 changed club,
-    #But data for season 2021/2022 weren't available. So to have more data, we took the data
-    #of season 2020/2021, but the league was different from the CURRENT LEAGUE. For this reason,
-    #In this column we put the LEAGUE name where the data took place.
-    
+    players_data$Data_League = substr(players_data$scouting_period,11,nchar(players_data$scouting_period))
+    players_data$scouting_period = substr(players_data$scouting_period,1,9)
+
     #Append the dataframes basing on position)
     if (isTRUE(dim(players_data)[2] >= 30) == TRUE){
-      if (isTRUE(players_data$Position=="Goalkeeper") == TRUE){
-        df_keepers = rbind(df_keepers, players_data)
-        } else {
+      if (isTRUE(players_data$Position[1] =="Goalkeeper") == TRUE){
+        df_keepers = rbind.fill(df_keepers, players_data)
+      } else {
         df_players = rbind(df_players, players_data)
-        }
+      }
     } else {next}
   }, error = function(e){})
 }
 
-#Making Index the first column (Player Name)
-df_players = cbind(Player_Names = rownames(df_players), df_players)
-#Delete column of scouting period, it's useless
-df_players$scouting_period <- NULL
 #Rename rows
 rownames(df_players) = 1:nrow(df_players)
 #Rearrange the dataset
-df_players <- df_players %>% relocate(Player_valuation,
+df_players <- df_players %>% relocate(Player,
+                                      link_TRM,
+                                      scouting_period,
                                       Age,
                                       Height,
                                       Position,
                                       Foot,
-                                      Y_Contract_Left,
-                                      Actual_Club,
                                       Data_League,
                                       .before = Goals)
 
-#Making Index the first column (Keeper Name)
-df_keepers = cbind(Keeper_Names = rownames(df_keepers), df_keepers)
-#Delete column of scouting period, it's useless
-df_keepers$scouting_period <- NULL
 #Rename rows
 rownames(df_keepers) = 1:nrow(df_keepers)
 #Rearrange the dataset
-df_keepers <- df_keepers %>% relocate(Player_valuation,
+df_keepers <- df_keepers %>% relocate(Player,
+                                      link_TRM,
+                                      scouting_period,
                                       Age,
                                       Height,
                                       Position,
                                       Foot,
-                                      Y_Contract_Left,
-                                      Actual_Club,
                                       Data_League,
                                       .before = Goals.Against)
 
---
+++ Datasets/Players Statistics/Player_Valuation_Scraper.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import requests, re, ast
+import numpy as np
+
+data_play = pd.read_csv("df_players.csv", encoding='latin-1')
+data_keep = pd.read_csv("df_keepers.csv", encoding='latin-1')
+
+data_play = data_play.drop(['Unnamed: 0'], axis = 1)
+data_keep = data_keep.drop(['Unnamed: 0'], axis = 1)
+
+def player_val_scraper(dataset):
+    
+    dataset['link_TRM'] = dataset['link_TRM'].apply(lambda x : x.replace('profil', 'marktwertverlauf'))
+    dataset['Player_Valuation'] = 0
+
+    for i in range(len(dataset)):
+        anno = dataset['scouting_period'][i][-4:]
+        r = requests.get(dataset['link_TRM'][i], headers = {'User-Agent':'Mozilla/5.0'})
+        p = re.compile(r"'data':(.*)}\],")
+        s = p.findall(r.text)[0]
+        s = s.encode().decode('unicode_escape')
+        data = ast.literal_eval(s)
+        ww = []
+        for k in range(len(data)):
+            if data[k]['datum_mw'][-4:] == anno:
+                ww.append(data[k]['y'])
+        if len(ww) != 0:
+            dataset.loc[i,"Player_Valuation"] = np.mean(ww)
+        else:
+            dataset.loc[i,"Player_Valuation"] = data[-1]['y']
+    return dataset
+        
+data_play = player_val_scraper(data_play)
+data_keep = player_val_scraper(data_keep)
+
+column_to_move = data_play.pop("Player_Valuation")
+data_play.insert(2, "Player Valuation", column_to_move)
+column_to_move = data_keep.pop("Player_Valuation")
+data_keep.insert(2, "Player Valuation", column_to_move)
+
+data_play = data_play.drop(['link_TRM'], axis = 1)
+data_keep = data_keep.drop(['link_TRM'], axis = 1) 
+
+data_play.to_csv("df_players.csv", index = False, encoding='utf-8-sig')
+data_keep.to_csv("df_keepers.csv", index = False, encoding='utf-8-sig')
